ad51915c1d6269c458fb90ec07930eeb953e8887
JanuszProgramowania <damianwasiak49@gmail.com>
2022-11-26 17:11:48 +0100

    add farm template

---
+++ App/src/Controller/farmController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class farmController extends AbstractController
+{
+    #[Route('/farm', name: 'farm')]
+
+   
+    public function farn(): Response
+    { 
+        return $this->render('farm/index.html.twig');
+    }
+}
