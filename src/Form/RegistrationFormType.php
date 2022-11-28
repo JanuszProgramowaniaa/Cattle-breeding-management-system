4b3f6c629fceff045556e9869e847cf3b3e022e0
JanuszProgramowania <damianwasiak49@gmail.com>
2022-11-28 23:25:00 +0100

    register add class from bootstrap

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,6 +6,8 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,9 +19,15 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('lastname')
+            ->add('email', EmailType::class, [
+              'attr' => ['class' => 'form-control']  
+            ])
+            ->add('lastname', TextType::class, [
+                'attr' => ['class' => 'form-control']  
+              ])
             ->add('agreeTerms', CheckboxType::class, [
+                'attr' => ['class' => 'form-check-input' ],
+                'label'=>"Zaakceptuj warunki",
                 'mapped' => false,
                 'constraints' => [
                     new IsTrue([
@@ -31,7 +39,7 @@ class RegistrationFormType extends AbstractType
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
+                'attr' => ['autocomplete' => 'new-password', 'class' => 'form-control' ],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter a password',
